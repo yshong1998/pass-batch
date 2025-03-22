b525a5dcf72699b140b43bb1790ca53857a40c19
yshong1998 <yshong1998@gmail.com>
2025-03-22 13:03:20 +0900

    feat: Job, Step 세팅

--- src/main/java/com/fastcampus/pass/PassBatchApplication.java
+++ src/main/java/com/fastcampus/pass/PassBatchApplication.java
@@ -1,13 +1,45 @@
 package com.fastcampus.pass;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class PassBatchApplication {
 
-	public static void main(String[] args) {
+	private final JobBuilderFactory jobBuilderFactory;
+	private final StepBuilderFactory stepBuilderFactory;
+
+    public PassBatchApplication(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(PassBatchApplication.class, args);
 	}
+
+    @Bean
+    public Step passStep() {
+        return this.stepBuilderFactory.get("passStep")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("Execute PassStep");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Job passJob() {
+        return this.jobBuilderFactory.get("passJob")
+                .start(passStep())
+                .build();
+    }
 
 }
